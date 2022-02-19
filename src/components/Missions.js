afcba61425c9cc82a23fd2ec1fa234d23d49a48b
Matheus Vital Diniz de Souza <matheus@localhost.localdomain>
2022-02-18 21:36:14 -0300

    Adjust more lint again

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,12 +6,13 @@ import missions from '../data/missions';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid='missions'>
-        <Title headline= 'Missões' />
+      <div data-testid="missions">
+        <Title headline="Missões" />
         { missions.map(({ name, year, country, destination }) => (
-          <MissionCard key={ name } name={ name } year={ year } country={ country } destination={ destination } />
+          <MissionCard key={ name } name={ name } 
+          year={ year } country={ country } destination={ destination } />
         ))}
-      </div> 
+      </div>
     );
   }
 }
